e2268901b619f877164b7a97484a74613ebd6daa
xwp200e <373713036@qq.com>
2020-08-18 16:57:37 +0800

    UserController1

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -4,9 +4,15 @@ import com.project.Util.MD5Util;
 import com.project.pojo.Client;
 import com.project.service.IClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
+@CrossOrigin(origins = "*")
 @RestController
 public class UserController {
 
@@ -19,6 +25,15 @@ public class UserController {
     public String registerClient(Client client){
         // Client密码MD5加密
         client.setPass(md5Util.pass2MD5(client.getPass()));
-        return null;
+
+        if (clientService.saveClient(client) >= 1){
+            return "success";
+        }
+        return "fail";
+    }
+
+    @GetMapping("/getAll")
+    public List<Client> getAll() {
+        return clientService.finaAll();
     }
 }
