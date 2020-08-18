18e0c6d0283b1fc894b2c7679423b56a16d26d4c
xwp200e <373713036@qq.com>
2020-08-18 15:53:35 +0800

    添加dev 分支

---
+++ src/main/java/com/project/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.project.controller;
+
+import com.project.Util.MD5Util;
+import com.project.pojo.Client;
+import com.project.service.IClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private IClientService clientService;
+
+    private MD5Util md5Util = MD5Util.getInstance();
+
+    @PostMapping("/registerClient")
+    public String registerClient(Client client){
+        // Client密码MD5加密
+        client.setPass(md5Util.pass2MD5(client.getPass()));
+
+        clientService.saveClient(client)
+        return null;
+    }
+}
